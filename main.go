92f520f027c9a32a88241208d201aade65237d8c
Meno Abels <meno.abels@adviser.com>
2023-03-10 21:57:36 +0100

    chore: improve the http.client

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
+
+	"github.com/spf13/pflag"
 )
+
+type FetchState struct {
+	Bytes   []byte
+	Error   error
+	Fetched time.Time
+	Count   int32
+}
+type RuntimeState struct {
+	Token               []byte
+	CACert              []byte
+	Jwks                FetchState
+	OpenIdConfiguration FetchState
+	WeitGroup           sync.WaitGroup
+	HttpClient          *http.Client
+	Log                 *log.Logger
+	Config              *Config
+}
 
 type Config struct {
 	APIServer          string
@@ -21,48 +43,79 @@ type Config struct {
 	Port               int
 }
 
-func (c *Config) getToken() ([]byte, error) {
-	bytes, err := os.ReadFile(c.ServiceAccountPath + "/token")
+func getToken(serviceAccountPath string) ([]byte, error) {
+	bytes, err := os.ReadFile(serviceAccountPath + "/token")
 	if err != nil {
 		return nil, err
 	}
 	return []byte(strings.TrimSpace(string(bytes))), nil
 }
 
-func (c *Config) getCACert() ([]byte, error) {
-	return os.ReadFile(c.ServiceAccountPath + "/ca.crt")
+func getCACert(serviceAccountPath string) ([]byte, error) {
+	return os.ReadFile(serviceAccountPath + "/ca.crt")
 }
 
-func (c *Config) k8sApiGet(url string) (*http.Response, error) {
-	token, err := c.getToken()
-	if err != nil {
-		return nil, err
+func (rs *RuntimeState) observerLoop(cfg *Config) {
+	rs.Log.Printf("Starting observer loop")
+	out, _ := json.Marshal(cfg)
+	rs.Log.Printf("Config: %s", string(out))
+	rs.WeitGroup.Add(1)
+	first := true
+	for {
+		token, err := getToken(cfg.ServiceAccountPath)
+		if err != nil {
+			rs.Log.Fatalf("Failed to read token: %s", err)
+		} else {
+			rs.Token = token
+		}
+		caCert, err := getCACert(cfg.ServiceAccountPath)
+		if err != nil {
+			rs.Log.Fatalf("Failed to read token: %s", err)
+		}
+		if !bytes.Equal(caCert, rs.CACert) && rs.Token != nil && caCert != nil {
+			rs.CACert = caCert
+			caCertPool := x509.NewCertPool()
+			caCertPool.AppendCertsFromPEM(caCert)
+			rs.HttpClient = &http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{
+						RootCAs: caCertPool,
+					},
+				},
+			}
+			rs.Log.Printf("Token and CA cert updated, restarting observer loop")
+			if first {
+				rs.WeitGroup.Done()
+				first = false
+			}
+		}
+		if rs.Jwks.Fetched.Add(time.Minute).After(time.Now()) {
+			rs.Log.Printf("Jwsks fetched %d", rs.Jwks.Count)
+			rs.Jwks.Count = 0
+			rs.Jwks.Bytes = nil
+			rs.Jwks.Fetched = time.Time{}
+		}
+		if rs.OpenIdConfiguration.Fetched.Add(time.Minute).After(time.Now()) {
+			rs.Log.Printf("OpenIdConfiguration fetched %d", rs.OpenIdConfiguration.Count)
+			rs.OpenIdConfiguration.Count = 0
+			rs.OpenIdConfiguration.Bytes = nil
+			rs.OpenIdConfiguration.Fetched = time.Time{}
+		}
+		time.Sleep(time.Minute)
 	}
-	caCert, err := c.getCACert()
-	if err != nil {
-		return nil, err
-	}
+}
 
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
-
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs: caCertPool,
-			},
-		},
-	}
+func (rs *RuntimeState) k8sApiGet(url string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", string(token)))
-	return client.Do(req)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", string(rs.Token)))
+	return rs.HttpClient.Do(req)
 }
 
-func (c *Config) getJson(url string) (map[string]interface{}, error) {
-	res, err := c.k8sApiGet(url)
+func (rs *RuntimeState) getJson(url string) (map[string]interface{}, error) {
+	res, err := rs.k8sApiGet(url)
 	if err != nil {
 		return nil, err
 	}
@@ -81,63 +134,76 @@ func (c *Config) getJson(url string) (map[string]interface{}, error) {
 	return val, nil
 }
 
-func (c *Config) fetchOpenIDConfiguration() ([]byte, error) {
-	val, err := c.getJson(fmt.Sprintf("%s/.well-known/openid-configuration", c.APIServer))
-	if err != nil {
-		return nil, err
-	}
-	purl, err := url.Parse(c.PublicURL)
-	if err != nil {
-		return nil, err
-	}
-	val["issuer"] = purl.String()
-	purl.Path = "/openid/v1/jwks"
-	val["jwks_uri"] = purl.String()
-	return json.Marshal(val)
-}
-
-func (c *Config) fetchJwks() ([]byte, error) {
-	val, err := c.getJson(fmt.Sprintf("%s/openid/v1/jwks", c.APIServer))
-	if err != nil {
-		return nil, err
-	}
-
-	return json.Marshal(val)
-}
-
-func openidConfigurationHandler(cfg *Config) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		bytes, err := cfg.fetchOpenIDConfiguration()
+func (rs *RuntimeState) fetchOpenIDConfiguration() ([]byte, error) {
+	if rs.OpenIdConfiguration.Bytes == nil {
+		val, err := rs.getJson(fmt.Sprintf("%s/.well-known/openid-configuration", rs.Config.APIServer))
 		if err != nil {
+			return nil, err
+		}
+		purl, err := url.Parse(rs.Config.PublicURL)
+		if err != nil {
+			return nil, err
+		}
+		val["issuer"] = purl.String()
+		purl.Path = "/openid/v1/jwks"
+		val["jwks_uri"] = purl.String()
+		rs.OpenIdConfiguration.Bytes, rs.OpenIdConfiguration.Error = json.Marshal(val)
+		rs.Jwks.Fetched = time.Now()
+	}
+	return rs.OpenIdConfiguration.Bytes, rs.OpenIdConfiguration.Error
+}
+
+func (rs *RuntimeState) fetchJwks() ([]byte, error) {
+	if rs.Jwks.Bytes == nil {
+		val, err := rs.getJson(fmt.Sprintf("%s/openid/v1/jwks", rs.Config.APIServer))
+		if err != nil {
+			return nil, err
+		}
+		rs.Jwks.Bytes, rs.Jwks.Error = json.Marshal(val)
+		rs.Jwks.Fetched = time.Now()
+	}
+	return rs.Jwks.Bytes, rs.Jwks.Error
+}
+
+func openidConfigurationHandler(rs *RuntimeState) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		bytes, err := rs.fetchOpenIDConfiguration()
+		if err != nil {
+			log.Printf("Failed to fetch openid configuration: %s", err)
 			w.WriteHeader(500)
-			fmt.Printf("Failed to fetch openid configuration: %s", err)
 			w.Write([]byte("Failed to fetch openid configuration"))
 			return
 		}
 		w.Write(bytes)
+		rs.OpenIdConfiguration.Count++
 	}
 }
 
-func jwksHandler(cfg *Config) func(w http.ResponseWriter, r *http.Request) {
+func jwksHandler(rs *RuntimeState) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		bytes, err := cfg.fetchJwks()
+		bytes, err := rs.fetchJwks()
 		if err != nil {
+			log.Printf("Failed to fetch jwks: %s", err)
 			w.WriteHeader(500)
-			fmt.Printf("Failed to fetch jwks: %s", err)
 			w.Write([]byte("Failed to fetch jwks configuration"))
 			return
 		}
 		w.Write(bytes)
+		rs.Jwks.Count++
 	}
 }
 
 func main() {
 	cfg := Config{}
-	flag.StringVar(&cfg.APIServer, "apiserver", "https://kubernetes.default.svc", "apiserver url")
-	flag.StringVar(&cfg.PublicURL, "publicUrl", "https://auth.mydomain.io", "public url of the auth server")
-	flag.StringVar(&cfg.ServiceAccountPath, "serviceAccountPath", "/var/run/secrets/kubernetes.io/serviceaccount", "base path of service account configuration")
-	flag.IntVar(&cfg.Port, "port", 80, "port to listen on")
-	flag.Parse()
+	rs := RuntimeState{
+		Log:    log.New(os.Stdout, "expose-oic", log.Lshortfile),
+		Config: &cfg,
+	}
+	pflag.StringVar(&cfg.APIServer, "apiserver", "https://kubernetes.default.svc", "apiserver url")
+	pflag.StringVar(&cfg.PublicURL, "publicUrl", "https://auth.mydomain.io", "public url of the auth server")
+	pflag.StringVar(&cfg.ServiceAccountPath, "serviceAccountPath", "/var/run/secrets/kubernetes.io/serviceaccount", "base path of service account configuration")
+	pflag.IntVar(&cfg.Port, "port", 80, "port to listen on")
+	pflag.Parse()
 	envPortStr, is := os.LookupEnv("PORT")
 	if is {
 		val, err := strconv.ParseInt(envPortStr, 10, 32)
@@ -147,8 +213,11 @@ func main() {
 			panic(fmt.Errorf("Invalid port in PORT environment variable"))
 		}
 	}
-	http.HandleFunc("/.well-known/openid-configuration", openidConfigurationHandler(&cfg))
-	http.HandleFunc("/openid/v1/jwks", jwksHandler(&cfg))
+	go rs.observerLoop(&cfg)
+	rs.WeitGroup.Wait()
+
+	http.HandleFunc("/.well-known/openid-configuration", openidConfigurationHandler(&rs))
+	http.HandleFunc("/openid/v1/jwks", jwksHandler(&rs))
 
 	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil)
 }
